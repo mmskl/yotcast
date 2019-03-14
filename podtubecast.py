e9d6135cd9eb6f5bacbdd75dd16cffb152dcaada
Marcin <mar@unix>
2019-03-14 03:05:48 +0100

    Turn off debug

--- podtubecast.py
+++ podtubecast.py
@@ -93,5 +93,5 @@ def download(video_id):
     return send_file(open(file_path, 'rb'), mimetype='application/octet-stream')
 
 if __name__ == '__main__':
-      app.run(host='localhost', port=5000, debug=True)
+      app.run(host='0.0.0.0', port=5000, debug=False)
 
